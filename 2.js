1e93550f00959546f9ddc0bf280da9cde4fa3abb
tszlamli <tszlamli@tencent.com>
2023-01-29 23:20:09 +0800

    feat: update

---
+++ 2.js
@@ -0,0 +1,26 @@
+// 函数实现
+const obj = {
+    a: {
+        ab: {
+            abb: 'a11',
+            ab: 'a12'
+        },
+        ad: {
+            a21: 'a21',
+            a2x: 'a22'
+        }
+    },
+    b: {
+        b_1: 'b1',
+        b_2: 'b2'
+    },
+    c: 'c'
+}
+function findPath() {
+    // 实现
+}
+
+findPath(obj, 'a22') // ['a', 'ab', 'a2x']
+
+
+// 这个应该是考算法实现

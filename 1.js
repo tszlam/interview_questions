0a3bc9020e00d61ec12f2384aef56a59b4801f62
tszlamli <tszlamli@tencent.com>
2023-01-30 09:48:19 +0800

    feat: update

--- 1.js
+++ 1.js
@@ -1,17 +1,35 @@
-const a = 1
-function foo() {
-    const a = 2
-    return function() {
-        console.log(a)
-    }
-}
-function bar() {
-    const a = 3
-    const func = foo()
-    return func()
-}
-const run = bar()
-run()
-
 // 这个应该是要说出输出的内容
 // 函数作用域的问题
+function q1() {
+    const a = 1
+    function foo() {
+        const a = 2
+        return function() {
+            console.log(a)
+        }
+    }
+    function bar() {
+        const a = 3
+        const func = foo()
+        return func()
+    }
+    const run = bar()
+    run()
+}
+
+// 变形
+function q2() {
+    const a = 1
+    function foo() {
+        return function() {
+            console.log(a)
+        }
+    }
+    function bar() {
+        const a = 3
+        const func = foo()
+        return func
+    }
+    const run = bar()
+    run()
+}

24fa94e6194d03d9f2e8e92010259f0605c3c9f1
tszlamli <tszlamli@tencent.com>
2023-02-07 22:20:34 +0800

    update ans

--- 2.js
+++ 2.js
@@ -26,3 +26,24 @@ findPath(obj, 'a22') // ['a', 'ab', 'a2x']
 // 这个应该是考算法实现
 
 //======================参考答案=======================
+// zl - 23.02.07
+function ans(obj, target) {
+    // 深度遍历
+    const o = {}
+    const find = (obj, target, path = []) => {
+        for (const key in obj) {
+            const val = obj[key]
+            if (val === target) {
+                return [...path, key]
+            }
+            if (typeof val !== 'object') continue // 不是object类型
+            if (val === null) continue // null特殊情况
+            if (Object.getPrototypeOf(o) !== Object.getPrototypeOf(val)) continue // 不是plain object
+            const res = find(val, target, [...path, key])
+            if (!res) continue
+            return res
+        }
+    }
+    const res = find(obj, target) || []
+    return res
+}

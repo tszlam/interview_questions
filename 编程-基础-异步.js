7b39c0c3e8f637f1a0128908e9789c781dbb83bc
celinefu <celinefu@tencent.com>
2023-02-10 10:41:39 +0800

    异步队列控制实现

--- 编程-基础-异步.js
+++ 编程-基础-异步.js
@@ -1,2 +1,21 @@
 // 编程实现
 // 异步队列控制: 有多个fetch请求，统一时间最多支持max个请求，全部请求完后将所有结果传给回调函数cb
+
+// celine 23.2.9
+async function asyncPool(tasks, max) {
+    const result = [];
+    const pool = new Set();
+    for (let i = 0; i < tasks.length; i++) {
+        const fetchTask = tasks[i];
+        pool.add(fetchTask);
+        result.push(fetchTask);
+        fetchTask().then(() => {
+            pool.delete(fetchTask);
+        });
+
+        if (pool.length >= max) {
+            await Promise.race();
+        }
+    }
+    return await Promise.all(result);
+}

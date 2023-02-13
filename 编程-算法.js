1b8d1edf1bb155461fa4961f57a2ba357689282e
celinefu <celinefu@tencent.com>
2023-02-13 22:23:47 +0800

    中序遍历

--- 编程-算法.js
+++ 编程-算法.js
@@ -1,3 +1,20 @@
 // 算法
 // Q1: 二叉树中序遍历
 // Q2: 数组的随机排序方法，要求概率一致
+
+// Q1 celine 23.2.13
+// leetcode https://leetcode.cn/problems/binary-tree-inorder-traversal/description/
+function mid(root) {
+    if (!root) return [];
+    const result = [];
+    const traverse = (node) => {
+        if (node.left) traverse(node.left);
+        result.push(node.val);
+        if (node.right) traverse(node.right);
+
+    }
+    traverse(root);
+    return result;
+}
+
+// Q2 celine 23.2.13

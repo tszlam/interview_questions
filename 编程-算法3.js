505127b3260e9c7988f1c46f672ebc9dc2b49a2f
celinefu <celinefu@tencent.com>
2023-02-14 15:00:38 +0800

    23.2.14 相同前缀

--- 编程-算法3.js
+++ 编程-算法3.js
@@ -2,3 +2,19 @@
 // 找出字符串数组中最长的相同前缀
 // ['flower', 'flow', 'flight'] => 'fl'
 // ['toutiao', 'douyin', 'tiktok'] => ''
+
+// celine 23.2.14
+// leetcode https://leetcode.cn/problems/longest-common-prefix/submissions/
+var longestCommonPrefix = function (strs) {
+    if (strs.length === 1) return strs[0];
+    let result = '';
+    const compare = strs[0];
+    for (let i = 0; i < compare.length; i++) {
+        if (strs.some(item => compare[i] !== item[i])) {
+            return result;
+        } else {
+            result += compare[i];
+        }
+    }
+    return result;
+};
